85b654c5120629993451f0c698fa7806ccd6e7d5
hli2020 <hongyangli2020@gmail.com>
2016-02-20 16:18:14 +0800

    minor update of the evaluation code

--- MatlabAPI/evalDemo.m
+++ MatlabAPI/evalDemo.m
@@ -1,41 +1,58 @@
 %% Demo demonstrating the algorithm result formats for COCO
+clc; close all; clear;
 
-%% select results type for demo (either bbox or segm)
-type = {'segm','bbox'}; type = type{1}; % specify type here
-fprintf('Running demo for *%s* results.\n\n',type);
+% select results type for demo (either bbox or segm)
+type = {'segm', 'bbox'};
+% specify type here
+type = type{2}; 
+fprintf('Running demo for *%s* results.\n\n', type);
 
-%% initialize COCO ground truth api
-dataDir='../'; dataType='val2014';
-annFile=sprintf('%s/annotations/instances_%s.json',dataDir,dataType);
-if(~exist('cocoGt','var')), cocoGt=CocoApi(annFile); end
+% initialize COCO ground truth api
+dataDir = '..'; 
+dataType = 'val2014';
+annFile = sprintf('%s/annotations/instances_%s.json', dataDir, dataType);
+if(~exist('cocoGt','var')) 
+    cocoGt = CocoApi(annFile); 
+end
 
-%% initialize COCO detections api
-resFile='%s/results/instances_%s_fake%s100_results.json';
-resFile=sprintf(resFile,dataDir,dataType,type);
-cocoDt=cocoGt.loadRes(resFile);
+% initialize COCO detections api
+resFile = '%s/results/instances_%s_fake%s100_results.json';
+resFile = sprintf(resFile, dataDir, dataType, type);
+cocoDt = cocoGt.loadRes(resFile);
 
 %% visialuze gt and dt side by side
-imgIds=sort(cocoGt.getImgIds()); imgIds=imgIds(1:100);
-imgId = imgIds(randi(100)); img = cocoGt.loadImgs(imgId);
-I = imread(sprintf('%s/images/val2014/%s',dataDir,img.file_name));
+imgIds = sort(cocoGt.getImgIds()); 
+imgIds = imgIds(1:100);
+im_id = imgIds(randi(100)); 
+im = cocoGt.loadImgs(im_id);
+I = imread(sprintf('%s/../val2014/%s', dataDir, im.file_name));
+
 figure(1); subplot(1,2,1); imagesc(I); axis('image'); axis off;
-annIds = cocoGt.getAnnIds('imgIds',imgId); title('ground truth')
-anns = cocoGt.loadAnns(annIds); cocoGt.showAnns(anns);
+% unique, instance-leve id
+ann_Ids = cocoGt.getAnnIds('imgIds', im_id); 
+title('ground truth');
+% structure
+gt_annos = cocoGt.loadAnns(ann_Ids); 
+cocoGt.showAnns(gt_annos);
+
 figure(1); subplot(1,2,2); imagesc(I); axis('image'); axis off;
-annIds = cocoDt.getAnnIds('imgIds',imgId); title('results')
-anns = cocoDt.loadAnns(annIds); cocoDt.showAnns(anns);
+ann_Ids = cocoDt.getAnnIds('imgIds', im_id); 
+title('results');
+res_annos = cocoDt.loadAnns(ann_Ids); 
+cocoDt.showAnns(res_annos);
 
 %% load raw JSON and show exact format for results
 fprintf('results structure have the following format:\n');
-res = gason(fileread(resFile)); disp(res)
+res = gason(fileread(resFile)); 
+disp(res);
 
-%% the following command can be used to save the results back to disk
-if(0), f=fopen(resFile,'w'); fwrite(f,gason(res)); fclose(f); end
+% the following command can be used to save the results back to disk
+if(0), f = fopen(resFile, 'w'); fwrite(f, gason(res)); fclose(f); end
 
 %% run Coco evaluation code (see CocoEval.m)
-cocoEval=CocoEval(cocoGt,cocoDt);
-cocoEval.params.imgIds=imgIds;
-cocoEval.params.useSegm=strcmp(type,'segm');
+cocoEval = CocoEval(cocoGt, cocoDt);
+cocoEval.params.imgIds = imgIds;
+cocoEval.params.useSegm = strcmp(type, 'segm');
 cocoEval.evaluate();
 cocoEval.accumulate();
 cocoEval.summarize();
